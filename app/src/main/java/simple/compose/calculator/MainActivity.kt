5a5006393025f261aa46a980efd0247505c293a2
simplepeng <383559698@qq.com>
2022-08-21 16:04:36 +0800

    适配横屏

--- app/src/main/java/simple/compose/calculator/MainActivity.kt
+++ app/src/main/java/simple/compose/calculator/MainActivity.kt
@@ -6,26 +6,28 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.*
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.simple.spiderman.SpiderMan
 import simple.compose.calculator.ui.theme.CalculatorComposeTheme
-import java.lang.ArithmeticException
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.util.Stack
+import java.util.*
 
 class MainActivity : ComponentActivity() {
 
